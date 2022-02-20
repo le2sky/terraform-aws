7611f10c473a1ebd5a8c9bc8b01d4765708e932c
leesky <leehaneul0623@gmail.com>
2022-02-20 15:17:41 +0900

    [migration] into the terraform-cloud

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -1,27 +1,44 @@
-
 //terraform setting
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 3.27"
-        }
+  cloud {
+    organization = "kukuluppingppong"
+    workspaces {
+      name = "terraform-aws"
     }
-    required_vesion = ">= 0.14.9"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  /*
+  Because the cloud block is not supported by older versions of Terraform,
+  you must use 1.1.0
+  */
+  required_version = ">= 1.1.0"
 }
 
-//configures the specified provider
-//A provider is a plugin that Terraform uses to create and manage your resources.
+/*
+configures the specified provider
+A provider is a plugin that Terraform uses to create and manage your resources.
+*/
 provider "aws" {
-    profile = "default"
-    region = "ap-northeast-2"
+  profile = "default"
+  region  = "ap-northeast-2"
 }
 
+/*
+define components of your infrastructure
+first string is resource type (provider_resource type)
+second string 2 is resource name (unique ID)
+*/
 resource "aws_instance" "app_server" {
-    ami = "ami-014009fa4a1467d53"
-    instance_type = "t2.micro"
+  ami           = "ami-014009fa4a1467d53"
+  instance_type = "t2.micro"
 
-    tags = {
-      Name = "Terraform tutorial App"
-    }
+  tags = {
+    Name = var.instance_name
+  }
 }
